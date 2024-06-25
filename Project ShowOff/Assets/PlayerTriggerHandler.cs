5bc7b59d1fabfd775edf5255099c1afbae248323
KingPengu <rutger2003@gmail.com>
2024-06-25 11:54:26 +0200

    Slight change to respawn

--- Project ShowOff/Assets/PlayerTriggerHandler.cs
+++ Project ShowOff/Assets/PlayerTriggerHandler.cs
@@ -8,7 +8,7 @@ public class PlayerTriggerHandler : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        UIManager.instance.respawnPoint = transform.position;
+        UIManager.instance.respawnPoint = transform.parent.position;
     }
 
     // Update is called once per frame
